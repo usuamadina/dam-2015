f54eeeb6b9a67a1d39ffd95e9a4cdc424bbdd7b9
Usua Madina <u.madina.i@gmail.com>
2015-03-02 19:34:37 +0100

    Ejercicio 2 HTML5, Reproductor de video

---
+++ HTML5/ej02/assets/js/app.js
@@ -0,0 +1,192 @@
+window.onload = function() {
+
+var loaded=false;
+var player = document.getElementById('player');
+var iniciar = document.getElementById('iniciar');
+var pausa = document.getElementById('pausa');
+var parar = document.getElementById('parar');
+var avanzar = document.getElementById('avanzar');
+var retroceder = document.getElementById('retroceder');
+var inicio = document.getElementById('inicio');
+var fin = document.getElementById('fin');
+var pcompleta = document.getElementById('pcompleta');
+var volume = document.getElementById('volume');
+var playlist= document.getElementById('playlist');
+
+
+
+
+var canPlayfn = function (e){
+    console.log('video loaded');
+    loaded=true;
+    volume.value=player.volume*100;
+    progress.value=0;
+};
+
+var playFn = function (e){
+    if(loaded){
+        player.play();
+        volume.value = player.volume*100;
+
+    }
+};
+
+var pauseFn = function(e){
+
+    if (loaded){
+        player.pause();
+        player.currentTime = 0;
+    }
+
+};
+var stopFn = function(e){
+    e.preventDefault();
+    if (loaded){
+        player.pause();
+        play.currentTime = 0;
+    }
+};
+
+var startFn = function (e){
+
+};
+
+var backwardFn = function(e){
+    e.preventDefault();
+    if (loaded){
+       
+        play.currentTime =-10;
+    }
+};
+
+var forwardFn = function(e){
+    e.preventDefault();
+    if (loaded){
+       
+        play.currentTime =+10;
+    }
+};
+
+var endFn = function(e){
+    e.preventDefault();
+    if (loaded){
+       
+        play.currentTime = player.duration;
+    }
+
+};
+
+var updateFn = function (e){
+    progress.value = player.curretnTime/player.duration*100;
+};
+
+var volumeFn = function (e){
+    if(loaded){
+        player.volume= this.value/100;
+    }
+};
+
+var changeVideo = function (e){
+    var src = target.dataset.src;
+
+   if (Modernizr.video.h264){
+    player.src = src + '.mp4';
+   }
+};
+
+
+
+
+
+
+
+
+
+
+
+    /*botones = document.querySelectorAll('boton');
+
+    for (var i = 0; i < botones.length; i++) {
+
+        if (botones[i].value === "Iniciar") {
+
+            iniciar(botones[i]);
+
+        } else if (botones[i].value === "Pausar"){
+
+            pausar(botones[i]);
+        }  else if (botones[i].value === "Avanzar"){
+
+            avanzar(botones[i]);
+        } else if (botones[i].value === "Pausar"){
+
+            pausar(botones[i]);
+
+        } else if (botones[i].value === "Retroceder"){
+
+            retroceder(botones[i]);
+
+        } else if (botones[i].value === "Fin"){
+
+            inicio(botones[i]);
+
+        } else if (botones[i].value === "Pantalla Completa"){
+
+            pausar(botones[i]);
+
+        }
+
+    }
+
+
+
+
+
+    var iniciar = function(e) {
+
+        this.play();
+    };
+
+    var pausa = function(e) {
+
+        this.pause();
+
+    };
+
+    /*var parar = function(e) {
+        this.
+
+    };
+
+    var avanzar = function(e) {
+
+    };
+
+    var retroceder = function(e) {
+
+    };
+
+    var fin = function(e) {
+
+    };
+
+    var completa = function(e) {
+
+    };*/
+
+
+
+
+
+    player.addEventListener('click', playFn, false);
+    play.addEventListener('click', playFn, false);
+    pause.addEventListener('click', PauseFn, false);
+    stop.addEventListener('click', StopFn, false);
+
+    document.addEventListener('canplay',canplayFn,false);
+    player.addEventListener('update', updateFn, false);
+    volume.addEventListener('input', volumeFn, false);
+
+
+
+};
